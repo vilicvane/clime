ddc7f557c531d1cd681756fe423750057bb918f6
Lion <maolion.j@gmail.com>
2016-11-05 20:16:38 +0800

    support assignment multiple commands root directory

---
+++ src/examples/multi-root/cli.ts
@@ -0,0 +1,10 @@
+import * as Path from 'path';
+import { CLI, Shim } from '../../';
+
+let cli = new CLI('greet', [
+    Path.join(__dirname, './commands'),
+    Path.join(__dirname, './extend-commands')
+]);
+
+let shim = new Shim(cli);
+shim.execute(process.argv);
---
+++ src/examples/multi-root/extend-commands/default.ts
@@ -0,0 +1,9 @@
+export const description = `extend commands`;
+
+export const subcommands = [
+    {
+        name: 'list',
+        alias: 'l',
+        brief: 'Show useless message of listting'
+    }
+];
