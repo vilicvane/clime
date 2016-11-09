e8959f0e1faaffbc155f06be7ea3802b406fe32a
Lion <maolion.j@gmail.com>
2016-11-09 19:46:27 +0800

    support assignment multiple commands root directory

---
+++ src/examples/multi-root/commands/default.ts
@@ -0,0 +1,52 @@
+import 'source-map-support/register';
+
+import {
+    command,
+    option,
+    param,
+    Command,
+    Options,
+    ExpectedError
+} from '../../../';
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+const messageMap: {
+    [key: string]: string;
+} = {
+    en: 'Hello, {name}!',
+    zh: '你好, {name}!'
+};
+
+export class GreetingOptions extends Options {
+    @option({
+        flag: 'l',
+        default: 'en',
+        description: 'Language of greeting message.'
+    })
+    lang: string;
+}
+
+@command({
+    description: 'This is a command that prints greeting message.'
+})
+export default class extends Command {
+    execute(
+        @param({
+            name: 'yourName',
+            required: true,
+            description: 'Your loud name.'
+        })
+        name: string,
+
+        options: GreetingOptions
+    ) {
+        let lang = options.lang;
+
+        if (hasOwnProperty.call(messageMap, lang)) {
+            return messageMap[lang].replace('{name}', name);
+        } else {
+            throw new ExpectedError(`Language "${lang}" is not supported`);
+        }
+    }
+}
---
+++ src/examples/multi-root/extend-commands/default.ts
@@ -0,0 +1,13 @@
+
+export const subcommands = [
+    {
+        name: 'list',
+        alias: 'l',
+        brief: 'Show useless message of listting (EXTEND)'
+    },
+    {
+        name: 'view',
+        alias: ['show', 'lookup'],
+        brief: 'Show useless message of viewing (EXTEND) '
+    }
+];
