9d17ae6d0761e9cf7945fd7d7ba5b609d4c94c89
vilicvane <i@vilic.info>
2017-03-21 01:00:44 +0800

    Add test cases

---
+++ src/test/baseman/multi-level/case-3/cli.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import * as Path from 'path';
+import { CLI, Shim } from '../../../../';
+
+let cli = new CLI('multi-level-3', Path.join(__dirname, 'commands'));
+
+let shim = new Shim(cli);
+shim.execute(process.argv);
