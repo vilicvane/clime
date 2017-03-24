e062ba528edac0cab142cb7d9316b824ac031c48
vilicvane <i@vilic.info>
2017-03-25 00:21:14 +0800

    Add test cases

---
+++ src/test/baseman/single-level/case-7/commands/default.ts
@@ -0,0 +1,34 @@
+import {
+  Command,
+  Options,
+  command,
+  metadata,
+  option,
+} from '../../../../..';
+
+export class FooOptions extends Options {
+  @option({
+    description: 'guess what foo',
+    placeholder: 'yo',
+    flag: 'h',
+    required: true,
+  })
+  foo: string;
+}
+
+export class FooBarOptions extends FooOptions {
+  @option({
+    description: 'guess what bar',
+  })
+  bar: number;
+}
+
+@command({
+  description: 'Foo bar',
+})
+export default class extends Command {
+  @metadata
+  execute(options: FooBarOptions) {
+    return JSON.stringify(options, undefined, 2);
+  }
+}
