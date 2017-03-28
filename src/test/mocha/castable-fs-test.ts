9935feb4931e65293d38b95076243fc5879dd0d7
vilicvane <i@vilic.info>
2017-03-29 00:32:48 +0800

    Add castable fs tests

---
+++ src/test/mocha/castable-fs-test.ts
@@ -0,0 +1,65 @@
+import * as Path from 'path';
+
+import { Context } from '../..';
+
+import {
+  Directory,
+  File,
+} from '../../castable-object/fs';
+
+const SAMPLE_FILES_DIR = Path.join(__dirname, '../../../test/sample-files');
+const TEXT_FILE_NAME = 'file.txt';
+const JSON_FILE_NAME = 'file.json';
+const NON_EXISTENT_NAME = 'guess-what';
+const TEXT_FILE_PATH = Path.join(SAMPLE_FILES_DIR, TEXT_FILE_NAME);
+const JSON_FILE_PATH = Path.join(SAMPLE_FILES_DIR, JSON_FILE_NAME);
+const NON_EXISTENT_PATH = Path.join(SAMPLE_FILES_DIR, NON_EXISTENT_NAME);
+
+const context = new Context({
+  cwd: SAMPLE_FILES_DIR,
+  commands: ['clime'],
+});
+
+describe('Castable object `File`', () => {
+  let textFile: File;
+  let jsonFile: File;
+
+  it('should assert existence', async () => {
+    textFile = File.cast(TEXT_FILE_PATH, context);
+    jsonFile = File.cast(JSON_FILE_PATH, context);
+
+    await textFile.assert();
+    await jsonFile.assert(false).should.be.rejectedWith('already exists');
+
+    await File.cast(TEXT_FILE_NAME, context).assert(true);
+    await File.cast(NON_EXISTENT_NAME, context).assert(false);
+    await File.cast(NON_EXISTENT_PATH, context).assert().should.be.rejectedWith('does not exist');
+    await File.cast(SAMPLE_FILES_DIR, context).assert().should.be.rejectedWith('expected to be a file');
+  });
+
+  it('should read buffer', async () => {
+    let buffer = await textFile.buffer();
+    buffer.should.be.instanceOf(Buffer);
+    buffer.toString('utf-8').should.equal('content\n');
+  });
+
+  it('should read text', async () => {
+    (await textFile.text('ascii')).should.equal('content\n');
+    (await textFile.text()).should.equal('content\n');
+  });
+
+  it('should require', () => {
+    jsonFile.require<object>().should.deep.equal({ key: 'value' });
+  });
+});
+
+describe('Castable object `Directory`', () => {
+  it('should assert existence', async () => {
+    await Directory.cast(SAMPLE_FILES_DIR, context).assert();
+    await Directory.cast('.', context).assert(true);
+    await Directory.cast(NON_EXISTENT_NAME, context).assert(false);
+    await Directory.cast(SAMPLE_FILES_DIR, context).assert(false).should.be.rejectedWith('already exists');
+    await Directory.cast(NON_EXISTENT_PATH, context).assert().should.be.rejectedWith('does not exist');
+    await Directory.cast(TEXT_FILE_NAME, context).assert().should.be.rejectedWith('expected to be a directory');
+  });
+});
