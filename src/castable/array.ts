de5c33e8ecdf7d49dadb0f24a971fe2dc63c869a
vilicvane <i@vilic.info>
2017-03-30 03:11:51 +0800

    Add castable array

---
+++ src/castable/array.ts
@@ -0,0 +1,57 @@
+import * as v from 'villa';
+
+import {
+  CastableType,
+  CastingContext,
+  GeneralValidator,
+  StringCastable,
+  buildCastingContext,
+  cast,
+} from '..';
+
+export interface ArrayCastingOptions<T> {
+  /** Separator to split the input string, defaults to ",". */
+  separator?: string | RegExp;
+  /** Whether to trim split strings before casting, defaults to `true`. */
+  trim?: boolean;
+  /** Whether to keep empty strings after split, defaults to `false`. */
+  empty?: boolean;
+  validator?: GeneralValidator<T>;
+  validators?: GeneralValidator<T>[];
+}
+
+export function array<T>(
+  type: CastableType<T>,
+  {
+    separator = ',',
+    trim = true,
+    empty = false,
+    validator,
+    validators,
+  }: ArrayCastingOptions<T> = {},
+): StringCastable<T[]> {
+  return {
+    async cast(str: string, context: CastingContext<T[]>): Promise<T[]> {
+      let parts = str.split(<any>separator);
+
+      if (trim) {
+        parts = parts.map(part => part.trim());
+      }
+
+      if (!empty) {
+        parts = parts.filter(part => !!part);
+      }
+
+      if (!validators) {
+        validators = validator ? [validator] : [];
+      }
+
+      let castingContext = buildCastingContext(context, {
+        name: `element of ${context.name}`,
+        validators,
+      });
+
+      return await v.map(parts, part => cast(part, type, castingContext));
+    },
+  };
+}
--- src/castable/index.ts
+++ src/castable/index.ts
@@ -1,2 +1,3 @@
+export * from './array';
 export * from './fs';
 export * from './miscellaneous';
