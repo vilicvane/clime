5da20dab83d8c2f68dd0f5cd195558224c49e5d4
vilicvane <i@vilic.info>
2017-03-31 21:54:03 +0800

    Add IP validators

---
+++ src/test/mocha/validation-network-test.ts
@@ -0,0 +1,37 @@
+import { ValidationContext } from '../..';
+
+import {
+  ip,
+  ipv4,
+  ipv6,
+} from '../../validation/network';
+
+describe('Network related validators', () => {
+  it('validator `ip` should work', () => {
+    ip('127.0.0.1', { name: 'test', source: '127.0.0.1' });
+    ip('::1', { name: 'test', source: '::1' });
+
+    (() => ip('abc', { name: 'test', source: 'abc' }))
+      .should.throw('Value (abc) of "test" is not a valid IP address');
+  });
+
+  it('validator `ipv4` should work', () => {
+    ipv4('127.0.0.1', { name: 'test', source: '127.0.0.1' });
+
+    (() => ipv4('abc', { name: 'test', source: 'abc' }))
+      .should.throw('Value (abc) of "test" is not a valid IPv4 address');
+
+    (() => ipv4('::1', { name: 'test', source: '::1' }))
+      .should.throw('Value (::1) of "test" is not a valid IPv4 address');
+  });
+
+  it('validator `ipv6` should work', () => {
+    ipv6('::1', { name: 'test', source: '::1' });
+
+    (() => ipv6('abc', { name: 'test', source: 'abc' }))
+      .should.throw('Value (abc) of "test" is not a valid IPv6 address');
+
+    (() => ipv6('127.0.0.1', { name: 'test', source: '127.0.0.1' }))
+      .should.throw('Value (127.0.0.1) of "test" is not a valid IPv6 address');
+  });
+});
---
+++ src/validation/network.ts
@@ -0,0 +1,24 @@
+import {
+  ExpectedError,
+  ValidatorFunction,
+} from '../core';
+
+import * as Net from 'net';
+
+export const ip: ValidatorFunction<string> = (value, { name }) => {
+  if (!Net.isIP(value)) {
+    throw new ExpectedError(`Value (${value}) of "${name}" is not a valid IP address`);
+  }
+};
+
+export const ipv4: ValidatorFunction<string> = (value, { name }) => {
+  if (!Net.isIPv4(value)) {
+    throw new ExpectedError(`Value (${value}) of "${name}" is not a valid IPv4 address`);
+  }
+};
+
+export const ipv6: ValidatorFunction<string> = (value, { name }) => {
+  if (!Net.isIPv6(value)) {
+    throw new ExpectedError(`Value (${value}) of "${name}" is not a valid IPv6 address`);
+  }
+};
