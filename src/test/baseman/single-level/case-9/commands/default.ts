a47788454a7e2954b58ab0235d6e32e79ff6e083
vilic <i@vilic.info>
2017-10-08 20:21:56 +0800

    Add test

---
+++ src/test/baseman/single-level/case-9/commands/default.ts
@@ -0,0 +1,24 @@
+import {
+  Command,
+  Options,
+  command,
+  metadata,
+  option,
+} from '../../../../..';
+
+export class SomeOptions extends Options {
+  @option({
+    required: true,
+  })
+  fooBar: number;
+}
+
+@command({
+  description: 'Foo bar',
+})
+export default class extends Command {
+  @metadata
+  execute(options: SomeOptions) {
+    return JSON.stringify(options, undefined, 2);
+  }
+}
---
+++ src/test/baseman/single-level/case-9/subcases.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'help-long',
+    args: ['--help'],
+  },
+  {
+    name: 'no-argument',
+    args: [],
+  },
+  {
+    name: 'valid-argument-1',
+    args: ['--foo-bar', '123'],
+  },
+  {
+    name: 'unknown-option-1',
+    args: ['--foo'],
+  },
+];
