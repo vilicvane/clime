1bacfcd562dbb0ef1746e341d39efb6d406b43c8
Dennis Ploeger <dennis.ploeger@kps.com>
2018-01-10 15:18:19 +0100

    Optimized, tests for non-string values

---
+++ src/test/baseman/single-level/case-nonstring-default-2/commands/default.ts
@@ -0,0 +1,26 @@
+import {
+  Command,
+  Options,
+  command,
+  metadata,
+  option,
+} from '../../../../..';
+
+export class SomeOptions extends Options {
+  @option({
+    required: true,
+    // tslint:disable-next-line:no-null-keyword
+    default: null,
+  })
+  fooBar: boolean;
+}
+
+@command({
+  description: 'Foo bar',
+})
+export default class extends Command {
+  @metadata
+  execute(options: SomeOptions) {
+    return JSON.stringify(options, undefined, 2);
+  }
+}
